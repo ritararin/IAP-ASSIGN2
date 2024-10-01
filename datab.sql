93654fc9423165dd649664eeecce58c6d92d2e58
ritararin <rita.palmeris@strathmore.edu>
2024-10-01 21:44:07 +0300

    database connection established

---
+++ datab.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE user_form;
+CREATE TABLE myusers(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,    
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP  
+);
+)
